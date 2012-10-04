3f31c190f021d4669522a00e2fcf47e0cbe5cf9a
Luta Project Admin <lutaproject@gmail.com>
2012-10-04 09:01:17 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Luta::Application.config.secret_token = 'e6d6ad028af3857370d5b31f61e9564720a1131045916bf7d9711440ad54309016bc0fd307e281dba45d1394c784484691e739569a89f8727df17dcb6839ef4c'
